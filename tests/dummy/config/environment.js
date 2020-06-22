03a2c2007a146df6fdde46066eb521b85efd0ec7
Chris Manson <chris@manson.ie>
2020-06-22 22:20:07 +0100

    add basic dev environment

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -20,6 +21,17 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    'field-guide': {
+      name: 'Field Guide',
+      copyright: '',
+
+      social: [{
+        name: 'github',
+        title: 'Field Guide Github',
+        link: 'https://github.com/empress/field-guide'
+      }]
     }
   };
 
