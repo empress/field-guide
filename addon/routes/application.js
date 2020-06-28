09fcc43a7f65534be2b2400b4d94caecc7a4294c
Chris Manson <chris@manson.ie>
2020-06-28 21:51:40 +0100

    support rootUrl other than /

--- addon/routes/application.js
+++ addon/routes/application.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
+import config from 'ember-get-config';
 
 export default Route.extend({
   model() {
-    return fetch(`/toc.json`)
+    return fetch(`${config.rootURL}toc.json`)
       .then(res => res.json())
   }
 })
--- addon/routes/show.js
+++ addon/routes/show.js
@@ -1,11 +1,12 @@
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
+import config from 'ember-get-config';
 
 export default Route.extend({
   model(params) {
     // remove trailing slash
     let path = params.path.replace(/\/$/, '');
-    return fetch(`/docs/${path}.json`)
+    return fetch(`${config.rootURL}docs/${path}.json`)
       .then(res => res.json())
       .then((res) => {
         return {
