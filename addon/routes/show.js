4ee0a6a5773a74ec9da901160c5c394de37fa3aa
Chris Manson <chris@manson.ie>
2021-09-16 11:32:48 +0100

    use router.transitionTo() in show route

--- addon/routes/show.js
+++ addon/routes/show.js
@@ -1,25 +1,27 @@
-/* eslint-disable ember/no-classic-classes, prettier/prettier */
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
 import config from 'ember-get-config';
+import { inject as service } from '@ember/service';
 
-export default Route.extend({
+export default class ShowRoute extends Route {
+  @service router;
+
   model(params) {
     // remove trailing slash
     let path = params.path.replace(/\/$/, '');
     return fetch(`${config.rootURL}docs/${path}.json`)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then((res) => {
         return {
           id: res.data.id,
           ...res.data.attributes,
-        }
+        };
       });
-  },
+  }
 
   redirect(model) {
-    if(model.id === 'index') {
-      this.transitionTo('index');
+    if (model.id === 'index') {
+      this.router.transitionTo('index');
     }
-  },
-})
+  }
+}
