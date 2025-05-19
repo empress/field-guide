93f54b4627b07e38b7c36829e7f9ad283f38f44f
Chris Manson <chris@manson.ie>
2025-05-19 11:13:25 +0100

    fix config for local dev

--- config/environment.js
+++ config/environment.js
@@ -21,17 +21,7 @@ module.exports = function (environment) {
     },
 
     'field-guide': {
-      name: 'Field Guide',
-      copyright: '',
-      host: 'https://field-guide.netlify.app/',
 
-      social: [
-        {
-          name: 'github',
-          title: 'Field Guide Github',
-          link: 'https://github.com/empress/field-guide',
-        },
-      ],
     },
 
     historySupportMiddleware: true,
---
+++ config/index.js
@@ -0,0 +1,17 @@
+export default function (/* environment */) {
+  return {
+    'field-guide': {
+      name: 'Field Guide',
+      copyright: '',
+      host: 'https://field-guide.netlify.app/',
+
+      social: [
+        {
+          name: 'github',
+          title: 'Field Guide Github',
+          link: 'https://github.com/empress/field-guide',
+        },
+      ],
+    },
+  };
+};
